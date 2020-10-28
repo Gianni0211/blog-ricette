7111c709c6e85337be80731673611372e8068334
Gianni Mondello <gianni.mondello@live.it>
2020-10-28 11:44:36 +0100

    fix sidebar tags singolo post

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -13,10 +13,14 @@
     <div class="box2 mt-3 border-bottom pb-4 mb-md-4">
     {{-- Aggiunge il titolo di tutti i post dell'utente che ha scritto il post --}}
       @foreach ($post->user->posts as $post)
-      <a href="{{ route('post.show', ['post' => $post]) }}" class="font-weight-bold text-primario h4 text-center mb-4">{{ $post->title }}</a>
+      <a href="{{ route('post.show', ['post' => $post]) }}" class="font-weight-bold text-primario h4 text-center mb-4 d-block">{{ $post->title }}</a>
           
       @endforeach  
-    
+    @foreach ($post->tags as $tag)
+        <div class="d-flex justify-content-start">
+        <i class="fas fa-tags text-primario "></i> <p class="test-secondario mx-4" >{{ $tag->name }}</p>
+      </div>
+    @endforeach
 
       {{-- <p>
         <i class="fas fa-utensils text-primario h4"></i> <a class="test-secondario" href="#!">tag</a>
